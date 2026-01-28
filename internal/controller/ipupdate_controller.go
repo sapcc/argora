36b94fe27137e2919f098f492945fa25abea922e
Anton Paulovich <a.paulovich@sap.com>
2026-01-28 13:03:05 +0100

    Find device and target interface by IP (#107)
    * Add func to find device name from IP
    
    * Changes motivated by code review
    
    * Strict regexp to find LAG interface
    
    * Fix linter warns
    
    * Update test

--- internal/controller/ipupdate_controller.go
+++ internal/controller/ipupdate_controller.go
@@ -5,12 +5,21 @@ package controller
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+
+	metalv1alpha1 "github.com/ironcore-dev/metal-operator/api/v1alpha1"
+	"github.com/sapcc/go-netbox-go/models"
+	ipamv1 "sigs.k8s.io/cluster-api/api/ipam/v1beta2"
 
 	"github.com/sapcc/argora/internal/credentials"
 	"github.com/sapcc/argora/internal/netbox"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	ipamv1 "sigs.k8s.io/cluster-api/api/ipam/v1beta2"
+
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
@@ -42,7 +51,9 @@ func (r *IPUpdateReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Complete(r)
 }
 
-// +kubebuilder:rbac:groups=ipam.cluster.x-k8s.io,resources=ipaddresses,verbs=list;watch;update;patch
+// +kubebuilder:rbac:groups=ipam.cluster.x-k8s.io,resources=ipaddresses,verbs=get;list;watch;update;patch
+// +kubebuilder:rbac:groups=ipam.cluster.x-k8s.io,resources=ipaddressclaims,verbs=get;list;watch
+// +kubebuilder:rbac:groups=metal.ironcore.dev,resources=serverclaims;servers,verbs=list;get;watch
 
 func (r *IPUpdateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
@@ -69,9 +80,114 @@ func (r *IPUpdateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		return ctrl.Result{}, err
 	}
 
+	deviceName, err := r.findDeviceName(ctx, req.Namespace, ipAddress)
+	if err != nil {
+		logger.Error(err, "unable to find device name")
+		return ctrl.Result{}, err
+	}
+
+	logger = logger.WithValues("deviceName", deviceName, "deviceIP", ipAddress.Spec.Address)
+	logger.Info("device found")
+
+	device, err := r.netBox.DCIM().GetDeviceByName(deviceName)
+	if err != nil {
+		logger.Error(err, "unable to find device by name")
+		return ctrl.Result{}, err
+	}
+
+	interfaces, err := r.netBox.DCIM().GetInterfacesForDevice(device)
+	if err != nil {
+		logger.Error(err, "unable to find interfaces by name")
+		return ctrl.Result{}, err
+	}
+
+	targetInterface, err := r.findTargetInterface(interfaces)
+	if err != nil {
+		logger.Error(err, "unable to find target interface")
+		return ctrl.Result{}, err
+	}
+
+	logger = logger.WithValues("interface", targetInterface.Name)
+	logger.Info("target interface found")
+
 	// ... perform update operations with r.netBox and ipAddress ...
 
 	logger.Info("reconcile completed successfully")
 
 	return ctrl.Result{}, nil
+}
+
+func (r *IPUpdateReconciler) findDeviceName(ctx context.Context, namespace string, ipAddr *ipamv1.IPAddress) (string, error) {
+	claimName := ipAddr.Spec.ClaimRef.Name
+
+	ipClaim := &ipamv1.IPAddressClaim{}
+	if err := r.k8sClient.Get(ctx, client.ObjectKey{Namespace: namespace, Name: claimName}, ipClaim); err != nil {
+		return "", fmt.Errorf("get IpAddressClaim: %w", err)
+	}
+
+	serverClaimName := getOwnerByKind(ipClaim.OwnerReferences, "ServerClaim")
+	if serverClaimName == "" {
+		return "", fmt.Errorf("no ServerClaim owner found for IpAddressClaim %s", ipClaim.Name)
+	}
+
+	serverClaim := &metalv1alpha1.ServerClaim{}
+	if err := r.k8sClient.Get(ctx, client.ObjectKey{Namespace: namespace, Name: serverClaimName}, serverClaim); err != nil {
+		return "", fmt.Errorf("get ServerClaim: %w", err)
+	}
+
+	if serverClaim.Spec.ServerRef == nil {
+		return "", fmt.Errorf("ServerClaim %s not yet bound to a server", serverClaimName)
+	}
+
+	server := &metalv1alpha1.Server{}
+	if err := r.k8sClient.Get(ctx, client.ObjectKey{Namespace: namespace, Name: serverClaim.Spec.ServerRef.Name}, server); err != nil {
+		return "", fmt.Errorf("get Server: %w", err)
+	}
+
+	if server.Spec.BMCRef.Name == "" {
+		return "", fmt.Errorf("server %s has no bmcRef name", server.Name)
+	}
+
+	return server.Spec.BMCRef.Name, nil
+}
+
+func getOwnerByKind(owners []metav1.OwnerReference, kind string) string {
+	for _, o := range owners {
+		if o.Kind == kind {
+			return o.Name
+		}
+	}
+
+	return ""
+}
+
+func (r *IPUpdateReconciler) findTargetInterface(interfaces []models.Interface) (models.Interface, error) {
+	var target models.Interface
+	maxLag := ""
+
+	re := regexp.MustCompile(`^LAG(\d)$`)
+
+	for _, iface := range interfaces {
+		if strings.ToLower(iface.Type.Value) != "lag" {
+			continue
+		}
+
+		m := re.FindStringSubmatch(iface.Name)
+		if len(m) != 2 {
+			continue
+		}
+
+		lag := m[1]
+
+		if lag > maxLag {
+			maxLag = lag
+			target = iface
+		}
+	}
+
+	if maxLag == "" {
+		return models.Interface{}, errors.New("no LAG interface found for device")
+	}
+
+	return target, nil
 }
--- internal/controller/ipupdate_controller_test.go
+++ internal/controller/ipupdate_controller_test.go
@@ -6,8 +6,11 @@ package controller
 import (
 	"context"
 
+	metalv1alpha1 "github.com/ironcore-dev/metal-operator/api/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/sapcc/go-netbox-go/models"
+	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -53,17 +56,110 @@ var _ = Describe("IP Update Controller", func() {
 			netBoxMock := &mock.NetBoxMock{
 				ReturnError:        false,
 				VirtualizationMock: &mock.VirtualizationMock{},
-				DCIMMock:           &mock.DCIMMock{},
-				IPAMMock:           &mock.IPAMMock{},
-				ExtrasMock:         &mock.ExtrasMock{},
+				DCIMMock: &mock.DCIMMock{
+					GetDeviceByNameFunc: func(deviceName string) (*models.Device, error) {
+						return &models.Device{
+							ID:   1,
+							Name: "node013-ap002",
+						}, nil
+					},
+					GetInterfacesForDeviceFunc: func(device *models.Device) ([]models.Interface, error) {
+						return []models.Interface{
+							{
+								Name: "LAG0",
+								Type: models.InterfaceType{Value: "lag"},
+							},
+							{
+								Name: "LAG1",
+								Type: models.InterfaceType{Value: "lag"},
+							},
+							{
+								Name: "eth0",
+								Type: models.InterfaceType{Value: "1000base-t"},
+							},
+						}, nil
+					},
+				},
+				IPAMMock:   &mock.IPAMMock{},
+				ExtrasMock: &mock.ExtrasMock{},
 			}
 
 			return netBoxMock
 		}
 
 		BeforeEach(func() {
+			By("create Server CR")
+			serverName := "test-server"
+			server := &metalv1alpha1.Server{}
+			err := k8sClient.Get(ctx, types.NamespacedName{Name: serverName, Namespace: resourceNamespace}, server)
+			if err != nil && apierrors.IsNotFound(err) {
+				resource := &metalv1alpha1.Server{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      serverName,
+						Namespace: resourceNamespace,
+					},
+					Spec: metalv1alpha1.ServerSpec{
+						SystemUUID: "test_uuid",
+						BMCRef: &v1.LocalObjectReference{
+							Name: "node013-ap002",
+						},
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+
+			By("create ServerClaim CR")
+			serverClaimName := "test-server-claim"
+			serverClaim := &metalv1alpha1.ServerClaim{}
+			err = k8sClient.Get(ctx, types.NamespacedName{Name: serverClaimName, Namespace: resourceNamespace}, serverClaim)
+			if err != nil && apierrors.IsNotFound(err) {
+				resource := &metalv1alpha1.ServerClaim{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      serverClaimName,
+						Namespace: resourceNamespace,
+					},
+					Spec: metalv1alpha1.ServerClaimSpec{
+						ServerRef: &v1.LocalObjectReference{
+							Name: serverName,
+						},
+						Image: "gardenlinux:latest",
+						Power: "On",
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+
+			By("create IPAM IPAddressClaim CR")
+			ipClaimName := "test-claim"
+			ipClaim := &ipamv1.IPAddressClaim{}
+			err = k8sClient.Get(ctx, types.NamespacedName{Name: ipClaimName, Namespace: resourceNamespace}, ipClaim)
+			if err != nil && apierrors.IsNotFound(err) {
+				resource := &ipamv1.IPAddressClaim{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      ipClaimName,
+						Namespace: resourceNamespace,
+						OwnerReferences: []metav1.OwnerReference{
+							{
+								APIVersion: "metal.ironcore.dev/v1alpha1",
+								Kind:       "ServerClaim",
+								Name:       serverClaimName,
+								UID:        "fake-uid",
+							},
+						},
+					},
+					Spec: ipamv1.IPAddressClaimSpec{
+						PoolRef: ipamv1.IPPoolReference{
+							Name:     "test-pool",
+							Kind:     "GlobalInClusterIPPool",
+							APIGroup: "ipam.cluster.x-k8s.io",
+						},
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+
 			By("create IPAM IPAddress CR")
-			err := k8sClient.Get(ctx, typeNamespacedUpdateName, ipAddress)
+			err = k8sClient.Get(ctx, typeNamespacedUpdateName, ipAddress)
 			if err != nil && apierrors.IsNotFound(err) {
 				resource := &ipamv1.IPAddress{
 					ObjectMeta: metav1.ObjectMeta{
